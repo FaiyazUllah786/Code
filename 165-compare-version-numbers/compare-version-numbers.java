8bcc81084942db521937d572314044c23f639b5c
Faiyaz Ullah <105494667+FaiyazUllah786@users.noreply.github.com>
2024-05-03 19:57:39 +0530

    Time: 1 ms (78.87%) | Memory: 41.4 MB (39.08%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -1,48 +1,17 @@
 class Solution {
-    public int compareVersion(String ver1, String ver2) {
-        List<Integer> v1 = new ArrayList<>();
-        List<Integer> v2 = new ArrayList<>();
-
-        StringBuilder temp = new StringBuilder();
-        for(int i=0;i<ver1.length();i++){
-            if(ver1.charAt(i)!='.') temp.append(ver1.charAt(i));
-            if(ver1.charAt(i)=='.' || i==ver1.length()-1){
-                v1.add(Integer.valueOf(temp.toString()));
-                temp.setLength(0);
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        for(int i=0;i<Math.max(v1.length,v2.length);i++){
+            int num1 = i<v1.length ? Integer.parseInt(v1[i]) : 0;
+            int num2 = i<v2.length ? Integer.parseInt(v2[i]) : 0;
+            if(num1<num2){
+                return -1;
+            }
+            if(num1>num2){
+                return 1;
             }
         }
-        temp.setLength(0);
-        for(int i=0;i<ver2.length();i++){
-            if(ver2.charAt(i)!='.') temp.append(ver2.charAt(i));
-            if(ver2.charAt(i)=='.' || i==ver2.length()-1){
-                v2.add(Integer.valueOf(temp.toString()));
-                temp.setLength(0);
-            }
-        }
-        int ans = 0;
-        while(!v1.isEmpty()&&!v2.isEmpty()){
-            int rev1 = v1.remove(0);
-            int rev2 = v2.remove(0);
-            if(rev1<rev2) return -1;
-            else if(rev1>rev2) return 1;
-            else ans = 0;
-        }
-
-        while(!v1.isEmpty()){
-            int rev1 = v1.remove(0);
-            if(rev1<0) return -1;
-            else if(rev1>0) return 1;
-            else ans = 0;
-        }
-
-        while(!v2.isEmpty()){
-            int rev2 = v2.remove(0);
-            if(0<rev2) return -1;
-            else if(0>rev2) return 1;
-            else ans = 0;
-        }
-
-        return ans;
-        
+        return 0;
     }
 }
