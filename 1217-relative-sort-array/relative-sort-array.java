8fb956f27f26ff070a1fbc328cfcf32839ed191d
Faiyaz Ullah <105494667+FaiyazUllah786@users.noreply.github.com>
2024-06-11 21:49:48 +0530

    Time: 4 ms (23.80%) | Memory: 42 MB (45.23%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.java
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -1,29 +1,26 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        Map<Integer,Integer> map = new HashMap<>();
+        Arrays.sort(arr1);
         List<Integer> li = new ArrayList<>();
         Set<Integer> set = new HashSet<>();
-        
-        for(int i:arr1){
-            map.put(i,map.getOrDefault(i,0)+1);
-        }
-        
-        for(int i:arr2){
+        for(int i: arr2){
             set.add(i);
-            int rep = map.get(i);
-            for(int j=0;j<rep;j++){
-                li.add(i);
+            for(int j=0;j<arr1.length;j++){
+                if(arr1[j]==i){
+                    li.add(i);
+                }
             }
         }
-        Arrays.sort(arr1);
         for(int i=0;i<arr1.length;i++){
-            if(!set.contains(arr1[i])) li.add(arr1[i]);
-        }
-        int[] ans = new int[li.size()];
-        for(int i=0;i<li.size();i++){
-            ans[i] = li.get(i);
+           if(!set.contains(arr1[i])) li.add(arr1[i]);
         }
         
+        int[] ans = new int[li.size()];
+        int idx = 0;
+        for(int i:li){
+            ans[idx++] = i;
+        }
         return ans;
+        
     }
 }
